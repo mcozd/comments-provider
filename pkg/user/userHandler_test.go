c5fe89cd96533c1099c415eeb80a23138ac7b160
Patrick Ostie <patrick.ostie@stroeer.de>
2020-03-09 23:20:22 +0100

    test: converted to gingko style

--- pkg/user/userHandler_test.go
+++ pkg/user/userHandler_test.go
@@ -3,21 +3,25 @@ package user
 import (
 	"net/http"
 	"net/http/httptest"
-	"testing"
+
+	. "github.com/onsi/ginkgo"
 )
 
-func TestUserFullInfoHandlerIsSuccessful(t *testing.T) {
-	req, err := http.NewRequest("GET", "/user/1", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+var _ = Describe("Testing with Ginkgo", func() {
+	It("user full info handler is successful", func() {
 
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(UserFullInfoHandler)
-	handler.ServeHTTP(rr, req)
+		req, err := http.NewRequest("GET", "/user/1", nil)
+		if err != nil {
+			GinkgoT().Fatal(err)
+		}
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v wanted %v",
-			status, http.StatusOK)
-	}
-}
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(UserFullInfoHandler)
+		handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusOK {
+			GinkgoT().Errorf("handler returned wrong status code: got %v wanted %v",
+				status, http.StatusOK)
+		}
+	})
+})
